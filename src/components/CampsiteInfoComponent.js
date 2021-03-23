83e72d604dff825f02ed2b072bcd9e9b10f4c592
Mauricio Bayas <mbayas1993@gmail.com>
2021-03-23 08:46:26 -0400

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -35,11 +35,10 @@ class CommentForm extends Component {
     });
   }
 
-  handleSubmit = (val) => {
-    alert("Thanks for submitting your comments: " + JSON.stringify(val));
-    console.log("Your comments: ", val);
+  handleSubmit(values) {
     this.toggleModal();
-  };
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+}
 
   render() {
     return (
@@ -136,7 +135,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId} ) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -157,7 +156,7 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
-        <CommentForm />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   }
@@ -182,7 +181,11 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments 
+            comments={props.comments}
+            addComment={props.addComment}
+            campsiteId={props.campsite.id}
+          />
         </div>
       </div>
     );
